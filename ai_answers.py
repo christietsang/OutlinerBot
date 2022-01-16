a58d631bfb6083cfd7ed084e17b87eb980810c23
Sepzie <81197023+Sepzie@users.noreply.github.com>
2022-01-15 22:10:51 -0800

    add test file

--- ai_answers.py
+++ ai_answers.py
@@ -2,11 +2,12 @@ import openai
 import ast
 
 # Insert API key here
-
+OPENAI_API_KEY = "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"
+OPENAI_ORGANIZATION = "org-d32asjx5v8BlNuI9aUwLDYeo"
 
 def get_data(file_name):
     try:
-        with open(file_name) as text_file:
+        with open(file_name, errors="ignore") as text_file:
             data = text_file.read()
     except FileNotFoundError:
         print(f"\"{file_name}\" does not exist.", )
@@ -18,7 +19,7 @@ def get_data(file_name):
 
 
 def answer_question(documents, question):
-    # openai.organization = OPENAI_ORGANIZATION
+    openai.organization = OPENAI_ORGANIZATION
     openai.api_key = OPENAI_API_KEY
 
     sample_outline = """Course Credits 5
@@ -34,10 +35,10 @@ def answer_question(documents, question):
     sample_questions = [["What grade do I need to pass this course?", "A minimum of 50%"],
                         ["What is the start date of this class?", "January 04, 2020"],
                         ["How much are the labs worth for this class?", "30%"]]
-
+    print(documents)
     result = openai.Answer.create(
-        search_model="ada",
-        model="curie",
+        search_model="davinci",
+        model="davinci",
         question=question,
         documents=documents,
         examples_context=sample_outline,
@@ -45,27 +46,29 @@ def answer_question(documents, question):
         max_tokens=50,
         stop=["\n", "<|endoftext|>"],
     )
-    answer = result["answers"][0]
-    score = result["selected_documents"][0]["score"]
-    if score > 200:
-        accuracy = "high"
-    elif score > 100:
-        accuracy = "medium"
-    elif score > 0:
-        accuracy = "low"
-    else:
-        accuracy = "negative"
+    print(result)
 
-    if accuracy == "negative":
-        return "Could not find an answer."
-    else:
-        return f"{answer}\nAccuracy: {accuracy}"
+    # answer = result["answers"][0]
+    # score = result["selected_documents"][0]["score"]
+    # if score > 200:
+    #     accuracy = "high"
+    # elif score > 100:
+    #     accuracy = "medium"
+    # elif score > 0:
+    #     accuracy = "low"
+    # else:
+    #     accuracy = "negative"
+    #
+    # if accuracy == "negative":
+    #     return "Could not find an answer."
+    # else:
+    #     return f"{answer}\nAccuracy: {accuracy}"
 
 
 def main():
-    documents = get_data("documents.txt")
-    question = "Does procedural have a midterm?"
-    print(answer_question(documents, question))
+    documents = get_data("course_outline.txt")
+    question = "How much is the final exam worth?"
+    print(answer_question([documents], question))
 
 
 if __name__ == '__main__':
