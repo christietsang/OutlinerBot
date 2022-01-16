742b02d767ab6feaa58acab1768d375f5354917a
Syte <belalk@live.ca>
2022-01-16 15:52:37 -0800

    Fix formatting

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 
 def generate_header(url: str) -> list[str, ...]:
-  """
+    """
   Generate a header string containing important course identifiers.
 
   :param url: a string
@@ -15,32 +15,32 @@ def generate_header(url: str) -> list[str, ...]:
   :postcondition: header is generated containing important information
   :return: a list containing important indentifying information
   """
-  page = requests.get(url)
-  # create beautiful soup object
-  soup = BeautifulSoup(page.text, "html.parser")
-  # identify HTML divs where data will be scraped
-  header_list = [
-      "outline__course-name",
-      "outline__course-code",
-      "outline__block outline__instructor-details"
-  ]
-  header = ""
-  # strip script tags from text in header
-  for index, element in enumerate(header_list):
-      header_info = soup.find('div', attrs={"class": element})
-      header += str(header_info.get_text())
+    page = requests.get(url)
+    # create beautiful soup object
+    soup = BeautifulSoup(page.text, "html.parser")
+    # identify HTML divs where data will be scraped
+    header_list = [
+        "outline__course-name",
+        "outline__course-code",
+        "outline__block outline__instructor-details"
+    ]
+    header = ""
+    # strip script tags from text in header
+    for index, element in enumerate(header_list):
+        header_info = soup.find('div', attrs={"class": element})
+        header += str(header_info.get_text())
 
-  new_header = []
-  # strip whitespace from text in header
-  for whitespace in header:
-      new_header.append(whitespace.replace("\n", " "))
+    new_header = []
+    # strip whitespace from text in header
+    for whitespace in header:
+        new_header.append(whitespace.replace("\n", " "))
 
-  new_header = ''.join(new_header)
-  return new_header
+    new_header = ''.join(new_header)
+    return new_header
 
 
 def generate_data(url: str, class_name: str) -> str:
-  """
+    """
   Generate a list of strings from a class in a webpage that can be parsed by OpenAI.
 
   :param url: a string
@@ -50,25 +50,25 @@ def generate_data(url: str, class_name: str) -> str:
   :postcondition: creates a string that contains the header followed by the information filtered from the class
   :return: a string containing the header followed by the information filtered from the class
   """
-  page = requests.get(url)
-  header = generate_header(url)
-  soup = BeautifulSoup(page.text, "html.parser")
-  new_data = []
+    page = requests.get(url)
+    header = generate_header(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    new_data = []
 
-  old_data = soup.find('div', attrs={"class": class_name})
-  parsed_data = str(old_data.get_text())
+    old_data = soup.find('div', attrs={"class": class_name})
+    parsed_data = str(old_data.get_text())
 
-  # clear whitespace in parsed data
-  for whitespace in parsed_data:
-      new_data.append(whitespace.replace("\n", " "))
-  new_data = str(''.join(new_data))
+    # clear whitespace in parsed data
+    for whitespace in parsed_data:
+        new_data.append(whitespace.replace("\n", " "))
+    new_data = str(''.join(new_data))
 
-  # join header with new data
-  return header + new_data
+    # join header with new data
+    return header + new_data
 
 
 def open_file(file_name: str) -> None:
-  """
+    """
   Open a file containing class names and facilitate data generation.
 
   :param file_name: a string
@@ -76,28 +76,28 @@ def open_file(file_name: str) -> None:
   :postcondition: facilitates creation of documents.txt containing all the filtered data for OpenAI
   :return: None 
   """
-  with open(file_name) as file_object:
-      # split URL links by line
-      lines = file_object.read().splitlines()
-      # identify divs where data will be scraped
-      class_names = [
-          "outline__block outline__details",
-          "outline__block outline__outline-sections",
-          "outline__block outline__course-description",
-          "outline__block outline__learning-outcomes"
-      ]
-      full_data_list = []
-      # append web-scraped data to an empty list
-      for url in lines:
-          for class_name in class_names:
-              full_data_list.append(generate_data(url, class_name))
-      name_file = "documents.txt"
-      # call function to write data to text file
-      add_to_txt(full_data_list, name_file)
+    with open(file_name) as file_object:
+        # split URL links by line
+        lines = file_object.read().splitlines()
+        # identify divs where data will be scraped
+        class_names = [
+            "outline__block outline__details",
+            "outline__block outline__outline-sections",
+            "outline__block outline__course-description",
+            "outline__block outline__learning-outcomes"
+        ]
+        full_data_list = []
+        # append web-scraped data to an empty list
+        for url in lines:
+            for class_name in class_names:
+                full_data_list.append(generate_data(url, class_name))
+        name_file = "documents.txt"
+        # call function to write data to text file
+        add_to_txt(full_data_list, name_file)
 
 
 def add_to_txt(data: list[str, ...], file_name: str) -> None:
-  """
+    """
   Write a text file that contains filtered, web-scraped data.
 
   :param data: a list of strings containing the web-scraped data
@@ -107,17 +107,17 @@ def add_to_txt(data: list[str, ...], file_name: str) -> None:
   :postcondition: creates a file that contains filtered, web-scraped data as a list with string elements
   :return: None
   """
-  #create text file and write web scraped data
-  with open(file_name, 'w', encoding="UTF-8",
-    errors="ignore") as file_object:
-    file_object.write(f"{data}")
+    # create text file and write web scraped data
+    with open(file_name, 'w', encoding="UTF-8",
+              errors="ignore") as file_object:
+        file_object.write(f"{data}")
 
 
 def generate_content() -> None:
-  """
+    """
   Begin scraping for OpenAI.await
   :precondition: links.txt exists in the same directory and contains a valid url on each line
   :postcondition: begin scraping for OpenAI using links.txt
   :return: None
   """
-  open_file("links.txt")
+    open_file("links.txt")
