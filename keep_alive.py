9beb851e7400140235576984815ed0af7241bbc1
Syte <belalk@live.ca>
2022-01-16 16:00:10 -0800

    Fix formatting

--- keep_alive.py
+++ keep_alive.py
@@ -10,13 +10,13 @@ app = Flask('')
 
 @app.route('/')
 def home():
-  return "Hello. I am alive and prepared for destruction!"
+    return "Hello. I am alive and prepared for destruction!"
 
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = Thread(target=run)
+    t.start()
--- main.py
+++ main.py
@@ -16,37 +16,37 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-  # await message.channel.send('We have logged in as {0.user}'.format(client))
-  print('We have logged in as {0.user}'.format(client))
+    # await message.channel.send('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
 
 
 @client.event
 #     message is received from user
 async def on_message(message):
-  if message.author == client.user:
-      return
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('$upload'):
-      links = message.content.replace('$upload\n', '')
-      with open('links.txt', 'w') as file_object:
-          file_object.write(links)
-      await message.channel.send("Links uploaded successfully!")
+    if message.content.startswith('$upload'):
+        links = message.content.replace('$upload\n', '')
+        with open('links.txt', 'w') as file_object:
+            file_object.write(links)
+        await message.channel.send("Links uploaded successfully!")
 
-  if message.content.startswith('$init'):
-      try:
-          generate_content()
-      except Exception:
-          await message.channel.send("Initialization failed!")
-      else:
-          await message.channel.send("Content generated successfully!")
+    if message.content.startswith('$init'):
+        try:
+            generate_content()
+        except Exception:
+            await message.channel.send("Initialization failed!")
+        else:
+            await message.channel.send("Content generated successfully!")
 
-  if message.content.startswith('$ask'):
-      question = message.content.replace("$ask ", "")
-      id = message.author.id
-      await message.channel.send(f'<@{id}>\n' + ask(question))
+    if message.content.startswith('$ask'):
+        question = message.content.replace("$ask ", "")
+        id = message.author.id
+        await message.channel.send(f'<@{id}>\n' + ask(question))
 
-  if message.content.startswith('$help'):
-      await message.channel.send("God helps those who help themselves")
+    if message.content.startswith('$help'):
+        await message.channel.send("God helps those who help themselves")
 
 
 keep_alive()
